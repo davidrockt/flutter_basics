1f7155a457de224b88268fb60d054a07ff2c8943
Waldmann, David <David.Waldmann@adesso.de>
2022-08-26 23:56:15 +0200

    Third Screen with containerized 2x2 Pictures

--- lib/second.dart
+++ lib/second.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basics/third.dart';
 
 class SecondScreen extends StatelessWidget {
   const SecondScreen({Key? key}) : super(key: key);
@@ -40,7 +41,9 @@ class SecondScreen extends StatelessWidget {
     return Scaffold(
       body: GestureDetector(
         onTap: () {
-          Navigator.pop(context);
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const ThirdScreen();
+          }));
         },
         child: Center(
             child: Column(
---
+++ lib/third.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class ThirdScreen extends StatelessWidget {
+  const ThirdScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter layout demo',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Third Screen'),
+          ),
+          body: _buildImageColumn(),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Icon(Icons.arrow_back),
+          ),
+        ));
+  }
+
+  Widget _buildImageColumn() {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.black26,
+      ),
+      child: Column(
+        children: [
+          _buildImageRow(1),
+          _buildImageRow(3),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDecoratedImage(int imageIndex) => Expanded(
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(width: 3, color: Colors.white),
+            borderRadius: const BorderRadius.all(Radius.circular(5)),
+          ),
+          margin: const EdgeInsets.all(8),
+          child: Image.network(
+            'https://picsum.photos/200',
+          ),
+        ),
+      );
+
+  Widget _buildImageRow(int imageIndex) => Row(
+        children: [
+          _buildDecoratedImage(imageIndex),
+          _buildDecoratedImage(imageIndex + 1),
+        ],
+      );
+}
