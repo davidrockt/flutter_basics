9967de5c261908aa6217fff4fa39b666028bda8d
Waldmann, David <David.Waldmann@adesso.de>
2022-08-26 22:47:41 +0200

    Images + Stars on Second Screen

--- lib/second.dart
+++ lib/second.dart
@@ -3,19 +3,49 @@ import 'package:flutter/material.dart';
 class SecondScreen extends StatelessWidget {
   const SecondScreen({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
+    var starsRow = Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        const Icon(Icons.star, color: Colors.black),
+        const Icon(Icons.star, color: Colors.black),
+      ],
+    );
+    var imagesRow = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(
+          child: Image.network(
+            'https://picsum.photos/210',
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Image.network(
+            'https://picsum.photos/210',
+          ),
+        ),
+        Expanded(
+          child: Image.network(
+            'https://picsum.photos/210',
+          ),
+        ),
+      ],
+    );
+
     return Scaffold(
       body: GestureDetector(
         onTap: () {
           Navigator.pop(context);
         },
         child: Center(
-          child: Image.network(
-            'https://picsum.photos/250?image=9',
-          ),
-        ),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [imagesRow, starsRow])),
       ),
       floatingActionButton: FloatingActionButton(
         tooltip: 'Add', // used by assistive technologies
@@ -26,5 +56,4 @@ class SecondScreen extends StatelessWidget {
       ),
     );
   }
-
 }
